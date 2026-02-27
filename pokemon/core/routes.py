12d5df3ece57b758884b62113006d41d9c168844
THIPOK08 <std.67122420209@ubru.ac.th>
2026-02-27 22:40:33 +0700

    021

--- pokemon/core/routes.py
+++ pokemon/core/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import current_user, login_required 
+from flask_login import current_user, login_required
 from pokemon.extensions import db, bcrypt 
 import sqlalchemy as sa
 
@@ -7,18 +7,16 @@ core_bp = Blueprint('core', __name__, template_folder='templates')
 
 @core_bp.route('/')
 def index():
-  page = request.args.get('page', type=int)   
-  pokemons = db.paginate(sa.select(Pokemon), per_page=4, page=page)
-  return render_template('core/index.html',
-                         title='Home Page',
-                         pokemons=pokemons)
+    page = request.args.get('page', type=int)
+    from pokemon.models import Pokemon 
+    pokemons = db.paginate(sa.select(Pokemon), per_page=4, page=page)
+    return render_template('core/index.html', title='Home Page', pokemons=pokemons)
 
 @core_bp.route('/<int:id>/detail')
 def detail(id):
-  pokemon = db.session.get(Pokemon, id)
-  return render_template('core/pokemon_detail.html',
-                         title='Pokemon Detail Page',
-                         pokemon=pokemon)
+    from pokemon.models import Pokemon
+    pokemon = db.session.get(Pokemon, id)
+    return render_template('core/pokemon_detail.html', title='Pokemon Detail Page', pokemon=pokemon)
 
 @core_bp.route('/change-password', methods=['GET', 'POST'])
 @login_required 
@@ -33,11 +31,11 @@ def change_password():
             return redirect(url_for('core.change_password'))
 
         if new_pw != confirm_pw:
-            flash('รหัสใหม่และยืนยันรหัสไม่ตรงกัน', 'danger')
+            flash('รหัสใหม่ไม่ตรงกัน', 'danger')
             return redirect(url_for('core.change_password'))
 
         hashed_pw = bcrypt.generate_password_hash(new_pw).decode('utf-8')
-        current_user.password = hashed_pw
+        current_user.password = hashed_pw 
         db.session.commit() 
 
         flash('เปลี่ยนรหัสผ่านสำเร็จแล้ว!', 'success')
