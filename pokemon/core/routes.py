a201a31dd8ba923a38374263cc7f5c687812d6f0
THIPOK08 <std.67122420209@ubru.ac.th>
2026-02-27 20:59:33 +0700

    09

--- pokemon/core/routes.py
+++ pokemon/core/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, sqlalchemy as sa
 from pokemon.extensions import db
 from pokemon.models import Pokemon, Type
 
